e44bf04d88b36d158adea60af2ba3b214e9082b6
Dhany <daniramadandaniramadan@gmail.com>
2024-09-24 09:02:11 +0800

    sadddd endingg

---
+++ tes_ruby.rb
@@ -0,0 +1,6 @@
+# Menerima input dari pengguna
+puts "Masukkan nama Anda:"
+nama = gets.chomp  # gets mengambil input dan chomp menghilangkan karakter newline
+
+# Menampilkan output
+puts "Halo, #{nama}! Selamat datang."
