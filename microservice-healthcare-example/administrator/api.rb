8107f2385b2a8d3efa868ab24c5b60de8232983a
Muhammad Dimas R. <draxozy@gmail.com>
2024-10-01 09:33:52 +0800

    db

--- microservice-healthcare-example/administrator/api.rb
+++ microservice-healthcare-example/administrator/api.rb
@@ -1,51 +1,49 @@
 require 'sinatra'
+require 'sinatra/activerecord'
 require 'json'
 
 puts "API.rb is running"
 
+set :database_file, 'config/database.yml'
+
+# Definisikan model Administrator
+class Administrator < ActiveRecord::Base
+end
+
 module HealthService
-  class Administrator < Sinatra::Base
+  class App < Sinatra::Base
     # Render halaman welcome
     get '/' do
       erb :index  # Menampilkan view index.erb
     end
 
-    # Menampilkan list administrator dalam format HTML
+    # Menampilkan list administrator dari database dalam format HTML
     get '/adm' do
-      @adms = [
-        {"id" => 1, "name" => "Irham Hasbi"},
-        {"id" => 2, "name" => "Dhany Ramadhan"},
-        {"id" => 3, "name" => "Muhammad Dimas"}
-      ]
+      @adms = Administrator.all
       erb :admin  # Menampilkan view admin.erb
     end
 
     # Menampilkan data administrator berdasarkan id
     get '/adm/:id' do
-      adms = [
-        {"id" => 1, "name" => "Irham Hasbi"},
-        {"id" => 2, "name" => "Dhany Ramadhan"},
-        {"id" => 3, "name" => "Muhammad Dimas"}
-      ]
-      @adm = adms.find { |adm| adm["id"] == params[:id].to_i }
+      @adm = Administrator.find_by(id: params[:id])
       
       if @adm
         erb :admin_detail  # Tambahkan view admin_detail.erb untuk detail admin
       else
         "Administrator tidak ditemukan"
+      end
+    end
 
-        # Route untuk memanggil service patient
-        get '/patients' do
-          content_type :json
-          begin
-            # Ganti URL di bawah dengan URL service "Patient" 
-            response = RestClient.get 'http://localhost:9293/patients'
-            response.body
-          rescue RestClient::ExceptionWithResponse => e
-            status 500
-            {error: "Gagal mengambil data pasien", message: e.message}.to_json
-          end
-        end
+    # Route untuk memanggil service patient
+    get '/patients' do
+      content_type :json
+      begin
+        # Ganti URL di bawah dengan URL service "Patient" 
+        response = RestClient.get 'http://localhost:9293/patients'
+        response.body
+      rescue RestClient::ExceptionWithResponse => e
+        status 500
+        {error: "Gagal mengambil data pasien", message: e.message}.to_json
       end
     end
   end
