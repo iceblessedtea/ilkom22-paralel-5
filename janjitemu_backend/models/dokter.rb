68e24d6fb92c6012c9878379aaa38e30ce219b3a
Seynz <elverandirham212@gmail.com>
2024-10-28 15:12:17 +0800

    Menambah models untuk janji temu

---
+++ janjitemu_backend/models/dokter.rb
@@ -0,0 +1,21 @@
+class Dokter
+  def self.create(nama, spesialis, usia)
+    DB.execute('INSERT INTO dokters (nama, spesialis, usia) VALUES (?, ?, ?)', nama, spesialis, usia)
+  end
+
+  def self.all
+    DB.execute('SELECT * FROM dokters')
+  end
+
+  def self.find(id)
+    DB.execute('SELECT * FROM dokters WHERE id = ?', id).first
+  end
+
+  def self.update(id, nama, spesialis, usia)
+    DB.execute('UPDATE dokters SET nama = ?, spesialis = ?, usia = ? WHERE id = ?', nama, spesialis, usia, id)
+  end
+
+  def self.delete(id)
+    DB.execute('DELETE FROM dokters WHERE id = ?', id)
+  end
+end
