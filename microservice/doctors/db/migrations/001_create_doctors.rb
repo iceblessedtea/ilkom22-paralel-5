7f04c5f0d7c29a4d1e7499127dcd42fea95b3864
NazwahIlmi <nazwahilmi05@gmail.com>
2024-12-14 17:06:18 +0800

    menambahkan db

--- microservice/doctors/db/migrations/001_create_doctors.rb
+++ microservice/doctors/db/migrations/001_create_doctors.rb
@@ -4,7 +4,6 @@ Sequel.migration do
       primary_key :id
       String :name, null: false
       String :specialization, null: false
-      String :phone
       DateTime :created_at, null: false
       DateTime :updated_at, null: false
     end
